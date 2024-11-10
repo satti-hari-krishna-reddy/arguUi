548df7f663eb60a3a72e8a76b2c3465bd29707ff
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-11-10 09:10:00 +0530

    adding next config

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	reactStrictMode: true,
+	env: {
+		dir: '/',
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**.vercel.app',
+				pathname: '/api/**'
+			},
+			{
+				protocol: 'https',
+				hostname: '**.githubusercontent.com',
+				pathname: '/**'
+			},
+		],
+	},
+}
