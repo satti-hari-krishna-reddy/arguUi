1460e169643343f7341749f0be018fd0beb63296
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-11-17 07:16:00 +0530

    add more components

---
+++ components/structure/container.jsx
@@ -0,0 +1,13 @@
+
+import css from '../../styles/structure/container.module.scss';
+
+export default function Container({ classProp, spacing, children }) {
+
+	const _class = classProp ? classProp : '';
+
+	return ( 
+		<div className={`${css.readingWidth} ${_class}`}>
+			{children}
+		</div>
+	);
+}
