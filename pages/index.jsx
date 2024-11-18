53cabacc54ec6f338afd328a719160007fdaa7d7
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-11-18 09:12:00 +0530

    update index

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Hero 		from '../components/sections/index/hero'
+import About 		from '../components/sections/index/about'
+import Technical 	from '../components/sections/index/technical'
+import Color 		from '../components/utils/page.colors.util'
+import colors 		from '../content/index/_colors.json'
+
+export default function HomePage() {
+
+	return (
+		<>
+			<Color colors={colors} />
+			<Hero />
+			<About />
+			<Technical />
+		</>
+	);
+}
